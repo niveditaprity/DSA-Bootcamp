12f6fdc75584b4d51bb4af7ce8acd74554455249
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2021-12-17 01:28:25 +0530

    BASIC

---
+++ Saummya/ARRAY-GFG-main/LEVEL1/Union of two arrays.py
@@ -0,0 +1,28 @@
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        a=set(a)
+        b=set(b)
+        
+        return len(a.union(b))
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
