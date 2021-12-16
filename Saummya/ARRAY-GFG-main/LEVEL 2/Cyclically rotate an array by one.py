3b3f231948cdd3c6632c62edb360bc8f462403f1
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2021-12-16 12:28:16 +0530

    Create  Cyclically rotate an array by one.py

---
+++ Saummya/ARRAY-GFG-main/LEVEL 2/Cyclically rotate an array by one.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+def rotate( arr, n):
+    
+    t= arr.pop()
+    arr.insert(0,t)
+    
+    return arr
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
