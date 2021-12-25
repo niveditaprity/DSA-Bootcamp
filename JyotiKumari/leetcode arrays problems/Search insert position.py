50e4a2bb326cc2eac78384a9f2f3170d118501bf
jaiswaljyoti <jaiswaljyoti2000@gmail.com>
2021-12-25 21:18:49 +0530

    https://leetcode.com/problems/search-insert-position/submissions/

---
+++ JyotiKumari/leetcode arrays problems/Search insert position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        
+        while start<= end:
+            mid =(start + end)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid-1
+                
+        return end + 1
