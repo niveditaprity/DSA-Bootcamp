82335802b2f6f3099af575853f6a435e045ac156
vinay25mit <61008587+vinay25mit@users.noreply.github.com>
2021-12-29 11:36:45 +0530

    Create Count_nodes.py

---
+++ vinay/Linked_list/Count_nodes.py
@@ -0,0 +1,60 @@
+
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    
+    #Function to count nodes of a linked list.
+    def getCount(self, head_node):
+        #code here
+        c=0
+        while(head_node):
+            c+=1
+            head_node=head_node.next
+        return c
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import io
+import sys
+
+    
+# Node class
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+#Linked list class
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    # append at the end of the list
+    def append(self,new_node):
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+            return
+        self.tail.next=new_node
+        self.tail = self.tail.next
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n=int(input())
+        a=LinkedList()
+        nodes=list(map(int,input().strip().split())) #list containing nodes
+        for x in nodes:
+            node=Node(x) # create a new node
+            a.append(node)
+        print(Solution().getCount(a.head))
+# } Driver Code Ends
