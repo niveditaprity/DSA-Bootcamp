b9058b51910381376a165bd4083904640d1ab01e
niki0718 <70174008+niki0718@users.noreply.github.com>
2022-01-05 00:55:04 +0530

    https://leetcode.com/problems/maximum-subarray/submissions/

--- Nikita/Array/day1/Maximum Subarray.py
+++ Nikita/Array/day1/Maximum Subarray.py
@@ -1,1 +1,10 @@
-.
+def maxSubArray(self, nums: List[int]) -> int:
+       
+        max1=nums[0]
+        sum=0
+        for n in nums:
+            if sum<0:
+                sum=0
+            sum+=n
+            max1=max(max1,sum)
+        return max1
