4cbe4e2d282dc7e00eda706a978979a0fd62984a
Saurabh Kumar Vigyananand <61475311+saurabhvigyananand@users.noreply.github.com>
2022-01-07 12:46:27 +0530

    Create find the frequency.py

---
+++ Saurabh Kr. Vigyananand/Array GFG/find the frequency.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+"""
+You're given an array (arr) of length n
+Return the frequency of element x in the given array
+"""
+def findFrequency (arr, n, x):
+    return arr.count(x)
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    n = int (input ())
+    arr = list (map (int, input ().split ()))
+    x = int (input ())
+    print (findFrequency (arr, n, x))
+    
+
+# } Driver Code Ends
