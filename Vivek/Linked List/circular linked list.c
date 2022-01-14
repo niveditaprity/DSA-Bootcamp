9c5efd16e67f25e50fa3ba4e3b828af77f76f631
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2022-01-14 19:31:06 +0530

    Linked list DAY 3

---
+++ Vivek/Linked List/circular linked list.c
@@ -0,0 +1,11 @@
+//No inner loops
+
+node *temp=head;
+ while(temp!=NULL)
+ { //if temp points to head then it has completed a circle,thus a circular linked list.
+    if(temp->next==head)
+        return true;
+    temp=temp->next;
+}
+
+return false;
