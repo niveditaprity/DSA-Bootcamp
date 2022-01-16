b2022522aa79d61c4e1c6e49eaf0c9d5561f9e9b
jaiswaljyoti <jaiswaljyoti2000@gmail.com>
2022-01-16 18:03:26 +0530

    https://leetcode.com/problems/sliding-window-maximum/submissions/

---
+++ JyotiKumari/Leetcode Queue problems/Sliding window maximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        hold = deque()
+        res = []
+        n = len(nums)
+        for i in range(k):
+            while hold and nums[i] >= nums[hold[-1]] :
+                hold.pop()
+            hold.append(i);
+        res.append(nums[hold[0]])
+        
+        for i in range(k, n):
+            while hold and hold[0] <= i-k:
+                hold.popleft()
+            while hold and nums[i] >= nums[hold[-1]] :
+                hold.pop()
+            hold.append(i)
+            res.append(nums[hold[0]])
+        
+        return res
