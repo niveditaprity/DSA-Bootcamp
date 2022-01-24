03fa38e530cb44b8411c5a0460a674182523b656
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-25 03:06:58 +0530

    https://leetcode.com/problems/binary-search/

---
+++ Saummya/BINARY SEARCH/Binary Search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        start=0
+        end=n-1
+        
+        while(start<=end):
+            mid=(start+end)//2
+            if (nums[mid]==target):
+                return mid
+            elif nums[mid]>target:
+                end=mid-1
+            else:
+                start=mid+1
+        return -1
+            
+     
+        
+      
