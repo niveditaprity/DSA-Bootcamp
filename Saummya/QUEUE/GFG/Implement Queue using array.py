ffac662f8624c09b50a7c22ea0aba251dec8ef84
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-25 03:00:45 +0530

    https://practice.geeksforgeeks.org/problems/implement-queue-using-array/1/?category[]=Queue&category[]=Queue&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]Queuedifficulty[]-2difficulty[]-1page1category[]Queue#

---
+++ Saummya/QUEUE/GFG/Implement Queue using array.py
@@ -0,0 +1,95 @@
+'''
+QUESTION:
+Implement a Queue using an Array. Queries in the Queue are of the following type:
+(i) 1 x   (a query of this type means  pushing 'x' into the queue)
+(ii) 2     (a query of this type means to pop element from queue and print the poped element)
+
+Example 1:
+
+Input:
+Q = 5
+Queries = 1 2 1 3 2 1 4 2
+Output: 2 3
+Explanation:
+In the first test case for query 
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the 
+    queue will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3 
+Example 2:
+
+Input:
+Q = 4
+Queries = 1 3 2 2 1 4   
+Output: 3 -1
+Explanation:
+In the second testcase for query 
+1 3 the queue will be {3}
+2   poped element will be 3 the
+    queue will be empty
+2   there is no element in the
+    queue and hence -1
+1 4 the queue will be {4}. 
+Your Task :
+You are required to complete the two methods push() which take one argument an integer 'x' to be pushed into the queue and pop() which returns a integer poped out from othe queue. If the queue is empty, it should return -1 on a pop operation. 
+
+Expected Time Complexity: O(1) for both push() and pop().
+Expected Auxiliary Space: O(1) for both push() and pop().
+
+Constraints:
+1 ≤ Q ≤ 105
+1 ≤ x ≤ 105
+'''
+
+#SOLUTION
+#User function Template for python3
+
+class MyQueue:
+    
+    def __init__(self):
+        self.items = []
+    
+    #Function to push an element x in a queue.
+    def push(self, x):
+         
+         #add code here
+         self.items.append(x)
+         
+         
+     
+    #Function to pop an element from queue and return that element.
+    def pop(self): 
+         
+         # add code here
+        if self.items:
+            return self.items.pop(0)
+        else:
+            return -1
+        
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyQueue()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+
+# } Driver Code Ends
