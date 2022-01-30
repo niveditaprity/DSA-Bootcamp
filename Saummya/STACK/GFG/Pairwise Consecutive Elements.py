8ccc8e6ade46677c646a5fa21ef2ec4dd79efe96
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-31 00:04:15 +0530

    https://practice.geeksforgeeks.org/problems/pairwise-consecutive-elements/1/?category[]=Stack&category[]=Stack&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]Stackdifficulty[]-2difficulty[]-1page1category[]Stack#

--- Saummya/STACK/GFG/Pairwise Consecutive Elements.py
+++ Saummya/STACK/GFG/Pairwise Consecutive Elements.py
@@ -7,7 +7,6 @@ def pairWiseConsecutive(l):
     for i in range(len(l)-1):
         if abs(l[i] - l[i+1]) > 1:
             return False
-    if len(l) > 1 and abs(l[-1] - l[-2]) > 1:
-        return False
+   
     
     return True
