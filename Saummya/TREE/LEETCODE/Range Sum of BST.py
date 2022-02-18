203bdc20b5e0dc218b55b432df2e33e752d77336
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-18 23:57:13 +0530

    easy

---
+++ Saummya/TREE/LEETCODE/Range Sum of BST.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        ans=0
+        if root is None:
+            return 0
+       
+        if root.val <= high and root.val >= low:
+            return root.val + self.rangeSumBST(root.right, low, high) +             self.rangeSumBST(root.left, low, high)
+        else:
+            return self.rangeSumBST(root.right, low, high) + self.rangeSumBST(root.left, low, high)
