c9aa4ebf5d7b8c632950f1d98e4cae06370ecefc
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-18 23:59:46 +0530

    easy

--- Saummya/TREE/LEETCODE/Range Sum of BST.py
+++ Saummya/TREE/LEETCODE/Range Sum of BST.py
@@ -14,3 +14,20 @@ class Solution:
             return root.val + self.rangeSumBST(root.right, low, high) +             self.rangeSumBST(root.left, low, high)
         else:
             return self.rangeSumBST(root.right, low, high) + self.rangeSumBST(root.left, low, high)
+
+        
+        #iterative approach
+        q = deque()
+        q.append(root)
+        res = 0 
+        
+        while q: # q = [10]
+            node = q.popleft()
+            if node.val >= low and node.val <= high:
+                res += node.val
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        
+        return res
